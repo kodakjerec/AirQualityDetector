c33eaf140d6c2c12e29bf7e4cb2ae8e4b12799c7
kodakjerec <kodakjerec@gmail.com>
2023-01-05 17:45:42 +0800

    updaye

--- AirQualityDetector.ino
+++ AirQualityDetector.ino
@@ -12,14 +12,14 @@
 AmebaST7735 tft = AmebaST7735(TFT_CS, TFT_DC, TFT_RST);
 
 // Assign human-readable names to some common 16-bit color values:
-#define	BLACK   0x0000
-#define	BLUE    0x001F
-#define	RED     0xF800
-#define	GREEN   0x07E0
-#define CYAN    0x07FF
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
 #define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
 
 // WiFi
 //SSID e senha da rede wifi para o ESP se conectar
@@ -47,15 +47,17 @@ long lastDebounceTime = 0;
 MQUnifiedsensor MQ2(placa, Voltage_Resolution, ADC_Bit_Resolution, pinMQ2, "MQ-2");
 
 // ** all data
-bool isCalibration = false;  // calibration Mode
-bool isLimit = false;        // add limit output
-float getMQ2_H2, getMQ2_LPG, getMQ2_CO, getMQ2_Alcohol, getMQ2_Propane, getMQ2_Voltage;
+bool isCalibration = false;                                                              // calibration Mode
+bool isLimit = false;                                                                    // add limit output
+float getMQ2_H2, getMQ2_LPG, getMQ2_CO, getMQ2_Alcohol, getMQ2_Propane, getMQ2_Voltage;  // MQ sensor
+// Wifi
+IPAddress ip = { 0, 0, 0, 0 };
 
 // 裝上啟動按鈕
 bool isEnableButton = false;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   // TFT
   tft.setResolution(128, 160);
   tft.begin();
@@ -63,7 +65,7 @@ void setup() {
   tft.clr();  //設定螢幕背景為黑色
 
   // WIFI
-  // wifi_setup();
+  wifi_setup();
 
   // MQ
   MQ_setup();
@@ -73,7 +75,6 @@ void setup() {
     pinMode(pinButton, INPUT_PULLUP);  // Button
   }
 }
-
 
 void loop() {
   // MQ
@@ -93,90 +94,101 @@ void loop() {
 // wifi設定
 void wifi_setup() {
   //Initialize serial and wait for port to open:
-  Serial.begin(115200);
+  int wiFiFailCount = 1; // fail count
   while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
+    ;
   }
 
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
+    tft.println("WiFi shield not present");
     // don't continue:
-    while (true)
-      ;
+    return;
   }
 
   // attempt to connect to Wifi network:
   while (status != WL_CONNECTED) {
-    Serial.println("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
+    tft.println("Attempting to connect to WPA SSID: ");
+    tft.println(ssid);
     // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
 
-    // wait 10 seconds for connection:
-    delay(10000);
+    // wait 5 seconds for connection:
+    
+    delay(1000);
+    tft.println(wiFiFailCount);
+    wiFiFailCount--;
+
+    if (wiFiFailCount<=0){
+      return;
+    }
   }
 
   // you're connected now, so print out the data:
-  Serial.println();
-  Serial.println("You're connected to the network");
+  tft.clr();
+  tft.setCursor(5, 7);
+  tft.println();
+  tft.println("You're connected to the network");
   printCurrentNet();
+  delay(5000);
   printWifiData();
+  delay(5000);
 }
 void printCurrentNet() {
   // print the SSID of the network you're attached to:
-  Serial.println("SSID: ");
-  Serial.println(WiFi.SSID());
+  tft.println("SSID: ");
+  tft.println(WiFi.SSID());
 
   // print the MAC address of the router you're attached to:
   byte bssid[6];
   WiFi.BSSID(bssid);
-  Serial.println("BSSID: ");
-  Serial.println(bssid[5], HEX);
-  Serial.println(":");
-  Serial.println(bssid[4], HEX);
-  Serial.println(":");
-  Serial.println(bssid[3], HEX);
-  Serial.println(":");
-  Serial.println(bssid[2], HEX);
-  Serial.println(":");
-  Serial.println(bssid[1], HEX);
-  Serial.println(":");
-  Serial.println(bssid[0], HEX);
+  tft.println("BSSID: ");
+  tft.print(bssid[5], HEX);
+  tft.print(":");
+  tft.print(bssid[4], HEX);
+  tft.print(":");
+  tft.print(bssid[3], HEX);
+  tft.print(":");
+  tft.print(bssid[2], HEX);
+  tft.print(":");
+  tft.print(bssid[1], HEX);
+  tft.print(":");
+  tft.print(bssid[0], HEX);
+  tft.println();
 
   // print the received signal strength:
   long rssi = WiFi.RSSI();
-  Serial.println("signal strength (RSSI):");
-  Serial.println(rssi);
+  tft.println("signal strength (RSSI):");
+  tft.println(rssi);
 
   // print the encryption type:
   byte encryption = WiFi.encryptionType();
-  Serial.println("Encryption Type:");
-  Serial.println(encryption, HEX);
-  Serial.println();
+  tft.println("Encryption Type:");
+  tft.print(encryption, HEX);
+  tft.println();
 }
 void printWifiData() {
   // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.println("IP Address: ");
-  Serial.println(ip);
-  Serial.println(ip);
+  ip = WiFi.localIP();
+  tft.println("IP Address: ");
+  tft.println(ip);
 
   // print your MAC address:
   byte mac[6];
   WiFi.macAddress(mac);
-  Serial.println("MAC address: ");
-  Serial.println(mac[0], HEX);
-  Serial.println(":");
-  Serial.println(mac[1], HEX);
-  Serial.println(":");
-  Serial.println(mac[2], HEX);
-  Serial.println(":");
-  Serial.println(mac[3], HEX);
-  Serial.println(":");
-  Serial.println(mac[4], HEX);
-  Serial.println(":");
-  Serial.println(mac[5], HEX);
+  tft.println("MAC address: ");
+  tft.print(mac[0], HEX);
+  tft.print(":");
+  tft.print(mac[1], HEX);
+  tft.print(":");
+  tft.print(mac[2], HEX);
+  tft.print(":");
+  tft.print(mac[3], HEX);
+  tft.print(":");
+  tft.print(mac[4], HEX);
+  tft.print(":");
+  tft.print(mac[5], HEX);
+  tft.println();
 }
 
 void MQ_setup() {
@@ -187,7 +199,7 @@ void MQ_setup() {
   if (isCalibration) {
     MQ2_Calibration();
   } else {
-    MQ2.setR0(0.4);
+    MQ2.setR0(0.61);
   }
 }
 // 重設MQ2
@@ -199,31 +211,31 @@ void MQ2_Calibration() {
   // We recomend executing this routine only on setup in laboratory conditions.
   // This routine does not need to be executed on each restart, you can load your R0 value from eeprom.
   // Acknowledgements: https://jayconsystems.com/blog/understanding-a-gas-sensor
-  Serial.println("Calibrating please wait 10 sec.");
+  tft.println("Calibrating please wait 10 sec.");
   float calR0 = 0;
   for (int i = 1; i <= 100; i++) {
     MQ2.update();
     calR0 += MQ2.calibrate(RatioMQ2CleanAir);
     if (i % 10 == 0) {
-      Serial.print(i / 10);
-      Serial.print(" ");
+      tft.print(i / 10);
+      tft.print(" ");
     }
     delay(100);
   }
-  Serial.println("MQ2 done!.");
+  tft.println("MQ2 done!.");
 
   if (isinf(calR0)) {
-    Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply");
+    tft.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply");
     return;
   }
   if (calR0 == 0) {
-    Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply");
+    tft.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply");
     return;
   }
   MQ2.setR0(calR0 / 100);
-  Serial.print("MQ2.getR0: ");
-  Serial.print(MQ2.getR0());
-  Serial.print(" | ");
+  tft.print("MQ2.getR0: ");
+  tft.print(MQ2.getR0());
+  tft.print(" | ");
   /*****************************  MQ CAlibration ********************************************/
   // MQ2.serialDebug(true);
 }
@@ -231,6 +243,8 @@ void MQ2_Calibration() {
 void MQ_loop() {
   MQ2.update();
   if (isCalibration) {
+    tft.print("MQ2.getR0: ");
+    tft.println(MQ2.getR0());
     MQ2.serialDebug();
   }
   // MQ2
@@ -249,7 +263,7 @@ void MQ_loop() {
   MQ2.setA(658.71);
   MQ2.setB(-2.168);
   getMQ2_Propane = MQ2.readSensor();
-  getMQ2_Voltage = analogRead(pinMQ2)*5.0/1023.0;
+  getMQ2_Voltage = analogRead(pinMQ2) * 5.0 / 1023.0;
   if (isLimit) {
     if (getMQ2_H2 < 300) getMQ2_H2 = 0;
     else if (getMQ2_H2 > 5000) getMQ2_H2 = 5000;
@@ -290,16 +304,16 @@ void button_loop() {
       ButtonStatus = reading;
       if (ButtonStatus == LOW) {
         if (isCalibration) return;
-        Serial.println("Start Calibration");
+        tft.println("Start Calibration");
         delay(2000);
         isCalibration = true;
         MQ2_Calibration();
         MQ2.update();
         MQ2.readSensor();
-        Serial.println();
+        tft.println();
         isCalibration = false;
       } else {
-        Serial.println("off");
+        tft.println("off");
       }
     }
   }
@@ -309,38 +323,85 @@ void button_loop() {
 // TFT
 bool isTftFirstTime = true;
 void tft_loop() {
-  if (isTftFirstTime){
-    tft.clr();  //設定螢幕背景為黑色
-    tft.setCursor(5, 7);  //設定文字游標點
+  if (isTftFirstTime) {
+    tft.clr();                 //設定螢幕背景為黑色
+    tft.setCursor(5, 7);       //設定文字游標點
     tft.setForeground(WHITE);  //設定文字顏色
-    tft.print("H2  :");printSpace(getMQ2_H2);tft.print(" ppm");
 
+    tft.print("H2  :");
+    printSpace(getMQ2_H2);
+    tft.print(" ppm");
     tft.setCursor(5, 21);
-    tft.print("LPG :");printSpace(getMQ2_LPG);tft.print(" ppm");
+    tft.print("LPG :");
+    printSpace(getMQ2_LPG);
+    tft.print(" ppm");
     tft.setCursor(5, 35);
-    tft.print("CO  :");printSpace(getMQ2_CO);tft.print(" ppm");
+    tft.print("CO  :");
+    printSpace(getMQ2_CO);
+    tft.print(" ppm");
     tft.setCursor(5, 49);
-    tft.print("ALCO:");printSpace(getMQ2_Alcohol);tft.print(" ppm");
+    tft.print("ALCO:");
+    printSpace(getMQ2_Alcohol);
+    tft.print(" ppm");
     tft.setCursor(5, 63);
-    tft.print("C3H8:");printSpace(getMQ2_Propane);tft.print(" ppm");
+    tft.print("C3H8:");
+    printSpace(getMQ2_Propane);
+    tft.print(" ppm");
     tft.setCursor(5, 77);
-    tft.print("VOLT:");printSpace(getMQ2_Voltage);tft.print(" V");
+    tft.print("VOLT:");
+    printSpace(getMQ2_Voltage);
+    tft.print(" V");
+
+    tft.setCursor(5, 91);
+    tft.println("IP Address: ");
+    tft.setForeground(YELLOW);  //設定文字顏色
+    tft.setCursor(5, 105);
+    tft.println(ip);
+    tft.setCursor(5, 112);
+    tft.println(getWiFiStatus());
+
     isTftFirstTime = false;
   } else {
-    tft.fillRectangle(35,7,63,7,BLACK);
-    tft.setCursor(35,7);printSpace(getMQ2_H2);
-    tft.fillRectangle(35,21,63,7,BLACK);
-    tft.setCursor(35,21);printSpace(getMQ2_LPG);
-    tft.fillRectangle(35,35,63,7,BLACK);
-    tft.setCursor(35,35);printSpace(getMQ2_CO);
-    tft.fillRectangle(35,49,63,7,BLACK);
-    tft.setCursor(35,49);printSpace(getMQ2_Alcohol);
-    tft.fillRectangle(35,63,63,7,BLACK);
-    tft.setCursor(35,63);printSpace(getMQ2_Propane);
-    tft.fillRectangle(35,77,63,7,BLACK);
-    tft.setCursor(35,77);printSpace(getMQ2_Voltage);
+    tft.setForeground(YELLOW);  //設定文字顏色
+    tft.fillRectangle(35, 7, 63, 7, BLACK);
+    tft.setCursor(35, 7);
+    printSpace(getMQ2_H2);
+    tft.fillRectangle(35, 21, 63, 7, BLACK);
+    tft.setCursor(35, 21);
+    printSpace(getMQ2_LPG);
+    tft.fillRectangle(35, 35, 63, 7, BLACK);
+    tft.setCursor(35, 35);
+    printSpace(getMQ2_CO);
+    tft.fillRectangle(35, 49, 63, 7, BLACK);
+    tft.setCursor(35, 49);
+    printSpace(getMQ2_Alcohol);
+    tft.fillRectangle(35, 63, 63, 7, BLACK);
+    tft.setCursor(35, 63);
+    printSpace(getMQ2_Propane);
+    tft.fillRectangle(35, 77, 63, 7, BLACK);
+    tft.setCursor(35, 77);
+    printSpace(getMQ2_Voltage);
   }
-  tft.setCursor(35,91);
-  tft.print("Height: ");tft.print(tft.getHeight());
-  tft.print("Width: ");tft.print(tft.getWidth());
+}
+// return wifi status-string
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+char* getWiFiStatus() {
+  //print the Wi-Fi status every 30 seconds
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    switch (WiFi.status()) {
+      case WL_NO_SSID_AVAIL:
+        return "Configured SSID cannot be reached";
+        break;
+      case WL_CONNECTED:
+        return "Connection successfully established";
+        break;
+      case WL_CONNECT_FAILED:
+        return "Connection failed";
+        break;
+    }
+    previousMillis = currentMillis;
+  }
+  return "";
 }
